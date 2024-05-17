3589cc26ecdc247a5a56178d8f46e9c91ea0ad18
BenjaminDearden <102755699+BenjaminDearden@users.noreply.github.com>
2024-05-17 12:57:54 -0400

    Add files via upload

---
+++ server.py
@@ -0,0 +1,22 @@
+import socket
+
+#Create a UDP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+#Bind the socket to port and address
+
+server_address = (socket.gethostbyname(socket.gethostname()), 12000)
+
+sock.bind(server_address)
+
+while True:
+    print("\nWaiting to receive message")
+    data, address = sock.recvfrom(4096)
+    
+    print("Received %s bytes from %s" % (len(data), address))
+    print("Data: %s" % data)
+    
+    if data:
+        sent = sock.sendto(data, address)
+        print("Sent %s bytes back to %s" % (sent, address))
+        
---
+++ udpclient.py
@@ -0,0 +1,21 @@
+import socket
+
+#create a socket object
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+server_address = (socket.gethostbyname(socket.gethostname()), 12000)
+message = b'HELLO'
+
+try:
+    #send data
+    print("Sending %s" % message)
+    sent = sock.sendto(message, server_address)
+    
+    #receive response
+    print("Waiting to receive")
+    data, server = sock.recvfrom(4096)
+    print("Received %s" % data)
+    
+finally:
+    print("Closing socket")
+    sock.close()
